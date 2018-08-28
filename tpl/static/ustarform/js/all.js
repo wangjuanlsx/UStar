dd10a4ea015b0fd6d8cbe7dae5ceac5f8b870152
sss <sss>
2018-08-28 22:46:20 +0800

    图片上传功能实现

--- tpl/static/ustarform/js/all.js
+++ tpl/static/ustarform/js/all.js
@@ -28,3 +28,69 @@ $("#picker-car-length").picker({
         }
     ]
 });
+
+/**/
+
+function getPhoto(node) {
+    var imgURL = "";
+    try{
+        var file = null;
+        if(node.files && node.files[0] ){
+            file = node.files[0];
+        }else if(node.files && node.files.item(0)) {
+            file = node.files.item(0);
+        }
+        //Firefox 因安全性问题已无法直接通过input[file].value 获取完整的文件路径
+        try{
+            imgURL =  file.getAsDataURL();
+        }catch(e){
+            imgRUL = window.URL.createObjectURL(file);
+        }
+    }catch(e){
+        if (node.files && node.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                imgURL = e.target.result;
+            };
+            reader.readAsDataURL(node.files[0]);
+        }
+    }
+    creatImg(imgRUL);
+    return imgURL;
+}
+
+function getPhoto1(node) {
+    var imgURL = "";
+    try{
+        var file = null;
+        if(node.files && node.files[0] ){
+            file = node.files[0];
+        }else if(node.files && node.files.item(0)) {
+            file = node.files.item(0);
+        }
+        //Firefox 因安全性问题已无法直接通过input[file].value 获取完整的文件路径
+        try{
+            imgURL =  file.getAsDataURL();
+        }catch(e){
+            imgRUL = window.URL.createObjectURL(file);
+        }
+    }catch(e){
+        if (node.files && node.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                imgURL = e.target.result;
+            };
+            reader.readAsDataURL(node.files[0]);
+        }
+    }
+    creatImg1(imgRUL);
+    return imgURL;
+}
+
+function creatImg(imgRUL){
+    $("#up-file-img1").attr("src",imgRUL);
+}
+
+function creatImg1(imgRUL){
+    $("#up-file-img2").attr("src",imgRUL);
+}
