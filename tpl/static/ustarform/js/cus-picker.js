28f2e34ec8a3084d73227ad275b9d54554c3d039
sss <sss>
2018-11-25 16:43:45 +0800

    修改

--- tpl/static/ustarform/js/cus-picker.js
+++ tpl/static/ustarform/js/cus-picker.js
@@ -9,12 +9,30 @@
         }
     });
 }*/
-$('input[type="text"],textarea').on('click', function () {
-    var target = this;
-    setTimeout(function(){
-        target.scrollIntoViewIfNeeded(true);
-    },0);
-});
+
+function jump_top(){
+    var u = navigator.userAgent;
+    var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
+    var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+    if(isAndroid){
+        $("body").css("height","0px");
+        $("body").css("height",document.body.scrollHeight);
+        $('#textArea').on("focus",function(event){
+            if($(event.target).attr("type")=="button")return;
+            $("body").css("margin-bottom","500px");
+            $(window).scrollTop($(event.target).offset().top-100);
+            event.preventDefault();
+        });
+        $("body").on("click",function(event){
+            if($(event.target).attr("type")=="button")return;
+            $("body").css("height","0px");
+            $("body").css("height",document.body.scrollHeight);
+            $("body").css("margin-bottom","0px");
+            event.preventDefault();
+        });
+    };
+}
+jump_top();
 
 function checkWords(input) {
     // 获取要显示已经输入字数文本框对象
