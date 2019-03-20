36688ab43f60f899ad7a8f73ba1674f71832fd7b
sss <sss>
2019-03-20 09:47:21 +0800

    修改

--- tpl/static/ustarform/js/new-order.js
+++ tpl/static/ustarform/js/new-order.js
@@ -10,4 +10,24 @@ $(function(){
         $('.new-detail-is-show-content').addClass('new-order-hidden');
         $('.open-btn').removeClass('new-order-hidden');
     })
-})
+});
+function checkItem(UserName,inputId,UserTrueName) {
+    var cbx = this.document.getElementById(UserName);
+    if (cbx.checked == true) {
+        this.document.getElementById(inputId).disabled=false;
+    } else {
+        this.document.getElementById(inputId).disabled=true;
+        this.document.getElementById(inputId).value = '';
+    }
+}
+//
+function OnInputWeight(inputId){
+    var z = document.getElementById("place11").value;
+    var a = document.getElementById("place3").value;
+    var b = document.getElementById("place4").value;
+    /*console.log('a=',a,'b=',b,'z=',z);*/
+    /*if(a&&b&&z){
+        var counst = z+a-b;
+        document.getElementById("span-id").innerText = counst;
+    }*/
+}
