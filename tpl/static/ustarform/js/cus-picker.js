fd1cbed5da4e87be0fd5aea47e1abb2d996e0eb2
slwang <470496305@qq.com>
2019-04-18 13:51:49 +0800

    commit

--- tpl/static/ustarform/js/cus-picker.js
+++ tpl/static/ustarform/js/cus-picker.js
@@ -114,7 +114,7 @@ function noopsycheFill() {
 /**
  *城市验证
  * */
-function CityValidation(addressInfo) {
+ function CityValidation(addressInfo) {
     var address = "";
     //省，自治区，直辖市
     getAllProvince();
@@ -965,7 +965,7 @@ function isPoneAvailable (pone) {
     }
 }
 /**
- *新增***判断是否为座机*/
+*新增***判断是否为座机*/
 function isPhone (pone) {
     var myreg1 = /^[0|4|9]{1,20}/;
     if (!myreg1.test(pone)) {
@@ -989,6 +989,133 @@ $('.submit').on('click',function(){
     /**姓名验证*/
     /*if(!isChinaName(chinaVal)){
      $.alert("请输入中文姓名!");
-     }*/
+ }*/
 });
+
+
+
+
+/*地图 自动识别 定位*/
+$('.site-icon').on('click',function(){
+    $.confirm('是否自动识别地址', function () {
+        getPresentLocal();
+        $('.map-lacation-wrap').css('display','block')
+        var siteListHeight = $(window).height() - $('.map-area').height();
+        $('.site-list-wrap').css('height',siteListHeight)
+    },function () {
+
+    })
+});
+$('.map-back-btn').on('click',function(){
+    $('.map-lacation-wrap').css('display','none')
+});
+// 创建地图实例  
+var map = new BMap.Map("container");
+// 创建点坐标  
+var point = new BMap.Point(116.404, 39.915);
+// 初始化地图，设置中心点坐标和地图级别  
+map.centerAndZoom(point, 16);
+//开启鼠标滚轮缩放
+map.enableScrollWheelZoom(true); 
+//定位
+var geolocation = new BMap.Geolocation();
+// 开启SDK辅助定位
+// 创建地理编码实例      
+var myGeo = new BMap.Geocoder();  
+
+function getPresentLocal() {
+    geolocation.enableSDKLocation();
+    geolocation.getCurrentPosition(function(r){
+        if(this.getStatus() == BMAP_STATUS_SUCCESS){
+            var mk = new BMap.Marker(r.point);
+            map.addOverlay(mk);
+            map.panTo(r.point);
+            console.log(r,'您的位置：'+r.point.lng+','+r.point.lat);
+            map.centerAndZoom(new BMap.Point(r.point.lng, r.point.lat), 18);      
+            // 根据坐标得到地址描述    
+            myGeo.getLocation(new BMap.Point(r.point.lng, r.point.lat), function(result){      
+                if (result){      
+                    console.log('地址',result);      
+                    displayPOI(result.point.lng,result.point.lat,1500);
+                }      
+            });
+        }
+        else {
+            alert('failed'+this.getStatus());
+        }        
+    });
+}    
+
+/*function displayPOI(site,lng,lat) {
+    map.clearOverlays();
+    var poiType = site;
+    var mPoint = new BMap.Point(lng, lat);
+    var circle = new BMap.Circle(mPoint,1500,{fillColor:"blue", strokeWeight: 1 ,fillOpacity: 1, strokeOpacity: 1});
+    var local =  new BMap.LocalSearch(map, {renderOptions: {map: map, autoViewport: false,panel: "site-list-wrap"}});
+    local.searchNearby('楼',mPoint,1500);
+    local.setMarkersSetCallback(function (result) {
+        console.log('resulttttttt',result,poiType)
+    })
+    console.log('local',circle)
+}*/
+var sites=[];
+var ponits_=[];//经纬度和地址信息
+function displayPOI(lng,lat,r){//参数：经lat、纬度lng、半径r
+    var mOption = {
+        poiRadius : r,           //半径为r米内的POI,
+        numPois : 10             //最多只有12个 系统决定的
+    }
+    var mPoint = new BMap.Point(lng, lat);
+    myGeo.getLocation(mPoint,
+        function mCallback(rs){
+            var sites = rs.surroundingPois; //获取全部POI(半径R的范围 最多12个点)
+            if(sites==null || sites==""){
+                return;
+            }
+            $(".site-list-wrap ul li").remove();
+            for(var i=0;i<sites.length;i++) {
+                var Point = new BMap.Point(sites[i].point.lng, sites[i].point.lat);
+                /*appendLacationPOI(Point,sites[i]);*/
+                var strHtml;
+                strHtml = "<li>";
+                strHtml += "<p>"+sites[i].title+"</p>";
+                strHtml += "<p>"+rs.addressComponents.city+' '+rs.addressComponents.district+"</p>";
+                strHtml += "</li>";
+                $(".site-list-wrap ul").append(strHtml);
+                /* console.log(sites[i].title)*/
+                console.log(Point)
+            }
+            $('.site-list-wrap ul').on('click',".site-list-wrap ul li",function(event){
+                console.log($(this).index() )
+                var i = $(this).index();
+                var address = sites[i].title;
+                $(".detail_textarea_wrap textarea").html(address);
+                $('.map-lacation-wrap').css('display','none');
+                var nameEl = document.getElementById('sel_city');
+                nameEl.value = rs.addressComponents.province+' '+rs.addressComponents.city+' '+rs.addressComponents.district;
+                postInfo.proName = rs.addressComponents.province;
+                postInfo.cityName = rs.addressComponents.city;
+                postInfo.disName = rs.addressComponents.district;
+            });
+            console.log(sites,ponits_);
+        },mOption
+        );          
+}
+
+function appendLacationPOI(res,site) {
+  var Point = res;
+  myGeo.getLocation(Point,function (result) {
+    var strHtml;
+    strHtml = "<li>";
+    strHtml += "<p>"+site.title+"</p>";
+    strHtml += "<p>"+result.addressComponents.city+' '+result.addressComponents.district+"</p>";
+    strHtml += "</li>";
+    $(".site-list-wrap ul").append(strHtml)
+    console.log(site,result)
+})
+}
+
+
+
+
 
